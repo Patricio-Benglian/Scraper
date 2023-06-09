896075ff25820846fc47d9ad14ca3aa98a58b934
Patricio-Benglian <6305@holbertonstudents.com>
2023-06-09 10:26:37 -0700

    Parse Prototypes

--- scraper.py
+++ scraper.py
@@ -2,34 +2,20 @@
 import re
 from bs4 import BeautifulSoup
 
-
-def protoParse(body):
-    row = task.find_all(string=("Prototype: "))
-    for r in row:
-        nextSib = r.nextSibling
-        return nextSib
-
-
 # open downloaded HTML file
 file = open("test.txt", "r+")
 # pass html file into beautiful soup, parse with html.parser
 soup = BeautifulSoup(file, "html.parser")
 
-list = []
-
+# grabs id name of all task divs
 taskList = [item["id"] for item in soup.find_all("div", id=re.compile("^task-num-"))]
-
+# iterates through id names and grabs entire html block, then parses values inside it
 for item in taskList:
     task = soup.find("div", {"id": item})
-    print(protoParse(task).text)
-    # prototype = task.find("code")
-    # print(prototype.text)
-
-
-# tasks = [item["data-role"] for item in soup.find_all("div", attrs={"data-role": True})]
-# for index, task in enumerate(tasks):
-#     children = soup.find_all("div #task-*")
-#     print(children)
+    prototype = task.find("li")
+    prototype = prototype.find("code")
+    if prototype:
+        print(prototype.text)
 
 
 # prototype = soup.find_all(string=("Prototype: "))
@@ -39,6 +25,7 @@ for item in taskList:
 
 
 # # Prototype Parser
+
 # row = soup.find_all(string=("Prototype: "))
 # for r in row:
 #     nextSib = r.nextSibling
@@ -47,6 +34,7 @@ for item in taskList:
 # print('~~~~~~~~~~~~~~~~~~~')
 
 # # Filename Parser
+
 # row = soup.find_all(string=('File: '))
 # for r in row:
 #     nextSib = r.nextSibling
@@ -55,6 +43,7 @@ for item in taskList:
 # print("~~~~~~~~~~~~~~~~~~~")
 
 # Terminal Parser
+
 # row = soup.find(string=re.compile('guillaume@ubuntu'))
 # # divides terminal example into newlines. Need to parse 'cat main' out of it, find filename after cat, and then copy all text after the cat up until
 # #the next terminal prompt
@@ -63,6 +52,8 @@ for item in taskList:
 # print (index)
 # print (row[index])
 
+
+## Get stuff that doesnt work
 
 # import requests
 # from requests.auth import HTTPBasicAuth
