f6cf293a4374c266c8c96582a5a61af89d51a4d1
Patricio-Benglian <6305@holbertonstudents.com>
2023-06-12 04:04:05 -0700

    Added Error message on no file found

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import re
+import sys
 from bs4 import BeautifulSoup
 from os import chmod
 
@@ -69,13 +70,18 @@ def createFile(prototype, fileName):
 
 
 # open downloaded HTML file
-file = open("test.txt", "r+")
+try:
+    file = open("test.txt", "r+")
+except FileNotFoundError:
+    print("test.txt not found :<")
+    sys.exit(1)
 # pass html file into beautiful soup, parse with html.parser
 soup = BeautifulSoup(file, "html.parser")
 file.close()
 
 # grabs id name of all task divs
-taskList = [item["id"] for item in soup.find_all("div", id=re.compile("^task-num-"))]
+taskList = [item["id"]
+            for item in soup.find_all("div", id=re.compile("^task-num-"))]
 # iterates through id names and grabs entire html block, then parses values inside it
 for item in taskList:
     task = soup.find("div", {"id": item})
@@ -87,7 +93,7 @@ for item in taskList:
     createMain(content, mainName)
     createFile(prototype, fileName)
 
-## Get stuff that doesnt work
+# Get stuff that doesnt work
 
 # import requests
 # from requests.auth import HTTPBasicAuth
