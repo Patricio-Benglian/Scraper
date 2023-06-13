e705c1150c5b8b09071527e14893dcf0346355e9
Patricio-Benglian <6305@holbertonstudents.com>
2023-06-13 06:32:07 -0700

    Now finds first item in list of filenames

--- scraper.py
+++ scraper.py
@@ -16,10 +16,9 @@ def fileParse(task):
     fileName = task.find("div", {"class": "list-group-item"})
     fileName = fileName.findAll("li")
     fileName = fileName[-1].text[6:]
-    if fileName.find(","):
+    if fileName.find(",") != -1:
         end = fileName.find(",")
         fileName = fileName[:end]
-    print(fileName)
     return fileName
 
 
